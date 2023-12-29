f206c403f302d2c12ca098c868c9c3c934fb0946
Tyler Carbone <tcarbone073@gmail.com>
2023-12-29 11:57:14 +0000

    Everything is prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
