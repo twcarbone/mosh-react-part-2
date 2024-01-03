f3fdc87c1ea4feeb91a6ea633bc9be3af4586e16
Tyler Carbone <tcarbone073@gmail.com>
2024-01-03 11:11:10 +0000

    react-query: infinite queries

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,27 +9,33 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 function usePosts(query: PostQuery) {
-  function fetchPosts() {
+  function fetchPosts({ pageParam = 1 }) {
     return axios
       .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
         params: {
-          _start: (query.page - 1) * query.pageSize,
+          _start: (pageParam - 1) * query.pageSize,
           _limit: query.pageSize,
         },
       })
       .then((res) => res.data);
   }
 
-  return useQuery<Post[], Error>({
+  return useInfiniteQuery<Post[], Error>({
     queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 10 * 1000,
     keepPreviousData: true,
+    getNextPageParam: (lastPage, allPages) => {
+      // In jsonplaceholder, anything beyond the last page is empty array
+      //
+      // A better API returns total number of records so we can compute when we will hit
+      // the last page.
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 }
 
--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 import usePosts from "../hooks/usePosts";
 
 const PostList = () => {
   const pageSize = 10;
-  const [page, setPage] = useState(1);
-  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -14,25 +14,22 @@ const PostList = () => {
   return (
     <>
       <ul className="list-group">
-        {posts.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
       <button
-        disabled={page === 1}
-        className="btn btn-primary my-3"
-        onClick={() => setPage(page - 1)}
-      >
-        Previous
-      </button>
-      <button
-        // We don't know how many total records, so can't disable
         className="btn btn-primary my-3 ms-1"
-        onClick={() => setPage(page + 1)}
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );
